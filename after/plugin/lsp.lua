941496dc650d797e3230755571a0d2b2568b45fd
thearturca <arti.1046@gmail.com>
2023-06-25 14:14:57 +0300

    some key bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -48,6 +48,8 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+      lsp.async_autoformat(client, bufnr)
 end)
 
 lsp.setup()
