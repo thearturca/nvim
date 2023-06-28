27c8919b55514706adc0f8d98f80506b3e550f56
thearturca <arti.1046@gmail.com>
2023-06-28 20:52:39 +0300

    added some new plugins

--- lua/thearturca/packer.lua
+++ lua/thearturca/packer.lua
@@ -4,62 +4,81 @@
 -- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+      -- Packer can manage itself
+      use 'wbthomason/packer.nvim'
 
-  use 'ThePrimeagen/harpoon'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+      use 'ThePrimeagen/harpoon'
+      use {
+            'nvim-telescope/telescope.nvim', tag = '0.1.1',
+            requires = { {'nvim-lua/plenary.nvim'} }
+      }
 
-  -- ColorScheme
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+      -- ColorScheme
+      use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-  -- Syntax highlight
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+      -- Syntax highlight
+      use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
 
-  use('mbbill/undotree')
-  use 'tpope/vim-fugitive'
+      use('mbbill/undotree')
+      use 'tpope/vim-fugitive'
 
-  -- LSP
-  use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-              -- LSP Support
-              {'neovim/nvim-lspconfig'},
-              {'williamboman/mason.nvim'},
-              {'williamboman/mason-lspconfig.nvim'},
+      -- LSP
+      use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v2.x',
+            requires = {
+                  -- LSP Support
+                  {'neovim/nvim-lspconfig'},
+                  {'williamboman/mason.nvim'},
+                  {'williamboman/mason-lspconfig.nvim'},
 
-              -- Autocompletion
-              {'hrsh7th/nvim-cmp'},
-              {'hrsh7th/cmp-buffer'},
-              {'hrsh7th/cmp-path'},
-              {'saadparwaiz1/cmp_luasnip'},
-              {'hrsh7th/cmp-nvim-lsp'},
-              {'hrsh7th/cmp-nvim-lua'},
+                  -- Autocompletion
+                  {'hrsh7th/nvim-cmp'},
+                  {'hrsh7th/cmp-buffer'},
+                  {'hrsh7th/cmp-path'},
+                  {'saadparwaiz1/cmp_luasnip'},
+                  {'hrsh7th/cmp-nvim-lsp'},
+                  {'hrsh7th/cmp-nvim-lua'},
 
-              -- Snippets
-              {'L3MON4D3/LuaSnip'},
-              {'rafamadriz/friendly-snippets'},
-        }
-  }
+                  -- Snippets
+                  {'L3MON4D3/LuaSnip'},
+                  {'rafamadriz/friendly-snippets'},
+            }
+      }
 
-  -- DAP
-  use 'mfussenegger/nvim-dap'
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-  use "jay-babu/mason-nvim-dap.nvim"
+      -- DAP
+      use 'mfussenegger/nvim-dap'
+      use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+      use "jay-babu/mason-nvim-dap.nvim"
 
-  -- bottom line
-  use {
+      -- bottom line
+      use {
             'nvim-lualine/lualine.nvim',
             requires = { 'nvim-tree/nvim-web-devicons', opt = true }
       }
 
-use {
-      'lewis6991/gitsigns.nvim',
-      -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-}
+      use {
+            'lewis6991/gitsigns.nvim',
+            -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+      }
+      -- AI Autocompletion
+      use {
+            'tzachar/cmp-tabnine',
+            run='./install.ps1'
+      }
+
+      -- Comments
+      use {
+            'numToStr/Comment.nvim',
+            config = function()
+                  require('Comment').setup()
+            end
+      }
+
+      -- autopairs
+      use {
+            "windwp/nvim-autopairs",
+            config = function() require("nvim-autopairs").setup {} end
+      }
 end)
 
