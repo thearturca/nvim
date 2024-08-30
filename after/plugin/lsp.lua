e64003a841eec809b01629041a8a406f2a518d96
thearturca <arti.1046@gmail.com>
2024-08-30 18:48:56 +0300

    fix(cmp): add mapping for complete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,6 +10,8 @@ local cmp_mappings = cmp.mapping.preset.insert({
       ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
       ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
 })
 
 cmp_mappings['<Tab>'] = nil
