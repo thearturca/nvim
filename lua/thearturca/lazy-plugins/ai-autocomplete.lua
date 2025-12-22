f144822e819ea22b2f10c4d121f7e8bbaba2eb56
thearturca <arti.1046@gmail.com>
2025-12-22 11:42:10 +0300

    feat(ai): remove tabnine

--- lua/thearturca/lazy-plugins/ai-autocomplete.lua
+++ lua/thearturca/lazy-plugins/ai-autocomplete.lua
@@ -1,22 +1,5 @@
 return {
 	{
-		"tzachar/cmp-tabnine",
-		build = "./install.ps1",
-		opts = {
-			max_lines = 1000,
-			max_num_results = 20,
-			sort = true,
-			run_on_every_keystroke = true,
-			snippet_placeholder = "..",
-			ignored_file_types = {
-				-- default is not to ignore
-				-- uncomment to ignore in lua:
-				-- lua = true
-			},
-			show_prediction_strength = false,
-		},
-	},
-	{
 		"Exafunction/windsurf.vim",
 		config = function()
 			vim.keymap.set("i", "<C-g>", function()
