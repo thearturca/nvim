2eb6b37e5025d2fdfec371247f8dd1616dd08cba
thearturca <arti.1046@gmail.com>
2026-01-12 16:34:49 +0300

    fix(leap): fix backward leap

--- lua/thearturca/lazy-plugins/leap.lua
+++ lua/thearturca/lazy-plugins/leap.lua
@@ -2,6 +2,6 @@ return {
 	url = "https://codeberg.org/andyg/leap.nvim",
 	config = function()
 		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
-		vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+		vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
 	end,
 }
